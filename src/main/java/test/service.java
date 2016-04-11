bb853c584948254c720f9a9510fd8c4e4c575a50
Archit1992 <gajjararchit00004@gmail.com>
2016-04-11 15:18:44 -0400

    Add one temporary service for making test case.

--- src/main/java/test/service.java
+++ src/main/java/test/service.java
@@ -1,5 +1,17 @@
 package main.java.test;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("user")
 public class service {
 
+	@GET
+	@Path("temp")
+	
+	public Response getString(){
+		return Response.status(200).entity("Hello Archit").build();
+	}
+	
 }
